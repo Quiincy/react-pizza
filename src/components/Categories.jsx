5976ac3ee1ac48cce11b0c831d42e5c21b004531
Quiincy <mishabelous11@gmail.com>
2021-11-26 18:06:02 +0200

    2nd commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+// class Categories extends React.Component{
+//     state = {
+//         activeItem: 2,
+//     };
+
+//     onSelectItem = index =>{
+//         this.setState({
+//             activeItem: index
+//         })
+//     }
+
+//     render(){
+//         const {items, onClick} = this.props;
+//         return(
+//             <div className="categories">
+//             <ul>
+
+//               {items.map((name, index) => <li
+//               className={this.state.activeItem === index ? 'active' : ''}
+//                onClick={() => this.onSelectItem(index)} 
+//                key={`${name}_${index}`}>{name}</li>)}
+//             </ul>
+//           </div>
+//         );
+//     }
+// }
+
+function Categories({items, onClick}) {
+    const [activeItem, setActiveItem] = React.useState(null);
+
+    const onSelectItem = (index) => {
+        setActiveItem(index)
+    }
+   
+
+    return (
+        <div className="categories">
+            <ul>
+              <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
+              {items && 
+                items.map((name, index) => (
+              <li
+                className={activeItem === index ? 'active' : ''}
+                onClick={() => onSelectItem(index)} 
+                key={`${name}_${index}`}>
+                {name}
+              </li>))}
+            </ul>
+          </div>
+    );
+}
+
+export default Categories;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
+import { Header } from './components';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+          <Route exact path="/" component={App} />  
+          <Route exact path="/qwer" component={Header} />  
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
