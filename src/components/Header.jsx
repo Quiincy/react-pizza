4f8c0cd0c13f0a65a6ec4323015db081f1521c1e
misha <mishabelous11@gmail.com>
2021-11-26 20:15:10 +0200

    react router has been inplemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logoSvg from "../assets/img/pizza-logo.svg"
 import Button from "./Button";
 
@@ -7,15 +8,18 @@ function Header() {
    return (
     <div className="header">
     <div className="container">
-      <div className="header__logo">
+     <Link to="/">
+     <div className="header__logo">
         <img width="38" src={logoSvg} alt="Pizza logo" />
         <div>
           <h1>React Pizza</h1>
           <p>самая вкусная пицца во вселенной</p>
         </div>
       </div>
+     </Link>
       <div className="header__cart">
-        <Button className="button--cart">
+       <Link to="/cart">
+       <Button className="button--cart">
         <span>520 ₽</span>
           <div className="button__delimiter"></div>
           <svg
@@ -49,10 +53,13 @@ function Header() {
           </svg>
           <span>3</span>
         </Button>
+       </Link>
       </div>
+
     </div>
   </div>
    );
 }
 
 export default Header;
+
--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
-import { Header } from './components';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-          <Route exact path="/" component={App} />  
-          <Route exact path="/qwer" component={Header} />  
-    </BrowserRouter>
+    <Router>
+          <App /> 
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
