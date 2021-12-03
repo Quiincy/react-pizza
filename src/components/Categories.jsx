a7eaed93322fee0961db7a89c0574089781c57b0
misha <mishabelous11@gmail.com>
2021-12-03 20:16:59 +0200

    pizzas added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 // class Categories extends React.Component{
 //     state = {
@@ -19,7 +19,7 @@ import React from 'react'
 
 //               {items.map((name, index) => <li
 //               className={this.state.activeItem === index ? 'active' : ''}
-//                onClick={() => this.onSelectItem(index)} 
+//                onClick={() => this.onSelectItem(index)}
 //                key={`${name}_${index}`}>{name}</li>)}
 //             </ul>
 //           </div>
@@ -27,29 +27,35 @@ import React from 'react'
 //     }
 // }
 
-function Categories({items, onClick}) {
-    const [activeItem, setActiveItem] = React.useState(null);
+function Categories({ items, onClick }) {
+  const [activeItem, setActiveItem] = React.useState(null);
 
-    const onSelectItem = (index) => {
-        setActiveItem(index)
-    }
-   
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
 
-    return (
-        <div className="categories">
-            <ul>
-              <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
-              {items && 
-                items.map((name, index) => (
-              <li
-                className={activeItem === index ? 'active' : ''}
-                onClick={() => onSelectItem(index)} 
-                key={`${name}_${index}`}>
-                {name}
-              </li>))}
-            </ul>
-          </div>
-    );
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeItem === null ? "active" : ""}
+          onClick={() => onSelectItem(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? "active" : ""}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}
+            >
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Categories;
